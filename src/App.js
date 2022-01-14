fe4f6c009637c9c5c217cef02f8acf572af2b9f5
MindpowerLLC <info@gotomindpower.com>
2022-01-13 20:03:39 -0800

    MORE

---
+++ src/App.js
@@ -0,0 +1,216 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+import * as s from "./styles/globalStyles";
+import styled from "styled-components";
+import i1 from "./assets/images/1.png";
+
+export const StyledButton = styled.button`
+  padding: 10px;
+  border-radius: 50px;
+  border: none;
+  background-color: #ffffff;
+  padding: 10px;
+  font-weight: bold;
+  color: #000000;
+  width: 300px;
+  cursor: pointer;
+  box-shadow: 2px 8px 4px -2px rgba(250, 250, 0, 0.5);
+  -webkit-box-shadow: 2px 3px 10px -2px rgba(250, 250, 0, 0.5);
+  -moz-box-shadow: 2px 8px 4px -2px rgba(250, 250, 0, 0.5);
+  :active {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+  }
+  :hover {
+    -webkit-box-shadow: 2px 3px 40px -2px rgba(250, 250, 0, 0.9);
+  }
+`;
+
+export const ResponsiveWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: stretched;
+  align-items: stretched;
+  width: 100%;
+  @media (min-width: 767px) {
+    flex-direction: column;
+  }
+`;
+
+export const StyledImg = styled.img`
+  width: 200px;
+  height: 200px;
+  @media (min-width: 767px) {
+    width: 300px;
+    height: 300px;
+  }
+  transition: width 0.5s;
+  transition: height 0.5s;
+`;
+
+function App() {
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [feedback, setFeedback] = useState("Time to get the flock out!");
+  const [claimingNft, setClaimingNft] = useState(false);
+
+  const claimNFTs = (_amount) => {
+    if (_amount <= 0) {
+      return;
+    }
+    setFeedback("Getting your Rude Bird ready...keep your pants on!");
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .mint(blockchain.account, _amount)
+      .send({
+        gasLimit: "285000",
+        to: "0x8EaFAf73fe95e82DbcDCd5222CeaF4fbc0aA5FE0",
+        from: blockchain.account,
+        value: blockchain.web3.utils.toWei((150 * _amount).toString(), "ether"),
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("What happen? Backing out?");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        setFeedback(
+          "Amazing, your Rude Bird has just been created. Visit Opensea.io to view your randomly generated NFT!"
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
+
+  return (
+    <s.Screen style={{ backgroundColor: "var(--black)" }}>
+      <s.Container flex={1} ai={"center"} style={{ padding: 12 }}>
+        <s.TextTitle
+          style={{ textAlign: "center", fontSize: 36, fontWeight: "bold" }}
+        >
+          Welcome to the Rude Birds minting station.
+          
+        </s.TextTitle>
+        <s.SpacerMedium />
+        <ResponsiveWrapper flex={1} style={{ padding: 12 }}>
+          <s.Container flex={1} jc={"center"} ai={"center"}>
+            <StyledImg alt={"example"} src={i1} />
+            <s.SpacerMedium />
+            <s.TextTitle
+              style={{ textAlign: "center", fontSize: 26, fontWeight: "bold" }}
+            >
+              {data.totalSupply}/10000
+            </s.TextTitle>
+          </s.Container>
+          <s.SpacerMedium />
+          <s.Container
+            flex={1}
+            jc={"center"}
+            ai={"center"}
+            style={{ backgroundColor: "#000000", padding: 12 }}
+          >
+            {Number(data.totalSupply) == 10000 ? (
+              <>
+                <s.TextTitle style={{ textAlign: "center" }}>
+                  The sale has ended.
+                </s.TextTitle>
+                <s.SpacerSmall />
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  You can still buy and trade Rude Bird NFTs on{""}
+                  <a
+                    target={""}
+                    href={"https://opensea.io/collection/Rude-Birds-One"}
+                  >
+                    Opensea.io
+                  </a>
+                </s.TextDescription>
+              </>
+            ) : (
+              <>
+                <s.TextTitle style={{ textAlign: "center" }}>
+                  1 Rude Birds NFT costs 150 MATIC + Polyon Gas Fees - Because ETH gas fees suck!
+                </s.TextTitle>
+                <s.SpacerXSmall />
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  -SAVE YOU SOME MONEY YO!-
+                </s.TextDescription>
+                <s.SpacerLarge />
+                <s.SpacerSmall />
+                <s.TextDescription style={{ textAlign: "center" }}>
+                  {feedback}
+                </s.TextDescription>
+                <s.SpacerMedium />
+                {blockchain.account === "" ||
+                blockchain.smartContract === null ? (
+                  <s.Container ai={"center"} jc={"center"}>
+                    <s.TextDescription style={{ textAlign: "center" }}>
+                    
+                    </s.TextDescription>
+                    <s.SpacerSmall />
+                    <StyledButton
+                      onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(connect());
+                        getData();
+                      }}
+                    >
+                      CONNECT WALLET
+                    </StyledButton>
+                    {blockchain.errorMsg !== "" ? (
+                      <>
+                        <s.SpacerSmall />
+                        <s.TextDescription style={{ textAlign: "center" }}>
+                          {blockchain.errorMsg}
+                        </s.TextDescription>
+                      </>
+                    ) : null}
+                  </s.Container>
+                ) : (
+                  <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                    <StyledButton
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        claimNFTs(1);
+                        getData();
+                      }}
+                    >
+                      {claimingNft ? "Let's Go!" : "Buy 1 NFT"}
+                    </StyledButton>
+                  </s.Container>
+                )}
+              </>
+            )}
+          </s.Container>
+        </ResponsiveWrapper>
+        <s.SpacerSmall />
+        <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
+          <s.TextDescription style={{ textAlign: "center", fontSize: 18 }}>
+            Rude Birds Community will be built upon progress of the Rude Birds NFT sales.<p>When floor price hits .5 ETH, we will distribute the 1st line of Member Mystery Box</p><p>filled with exclusive gear we know you'll love</p>
+          </s.TextDescription>
+          <s.SpacerSmall />
+          <s.TextDescription style={{ textAlign: "center", fontSize: 14 }}>
+            "This is our first line of NFTs - Nothing is ever easy, lessons learned - MCMLLC.io"
+          </s.TextDescription>
+        </s.Container>
+      </s.Container>
+    </s.Screen>
+  );
+}
+
+export default App;
